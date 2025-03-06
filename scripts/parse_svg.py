51310025ad368b0b1bbff47aa9ad07513d67a979
vlad-cool <vlad-matveev2004@mail.ru>
2025-03-06 22:34:36 +0300

    femtovg crutch added

--- scripts/parse_svg.py
+++ scripts/parse_svg.py
@@ -96,7 +96,7 @@ with open("src/layouts.rs", "w") as f:
             f.write(f"        y: {element.y - 100},\n")
             f.write(f"        width: {element.width + 200},\n")
             f.write(f"        height: {element.height + 200},\n")
-            f.write(f"        font_size: {element.font_size},\n")
+            f.write(f"        font_size: {element.font_size - 1},\n")
             f.write(f"    }},\n")
         if element.type == "rect":
             f.write(f"    {element.name}: RectangleProperties {{\n")
